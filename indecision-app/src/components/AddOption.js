4db2381f4ef765ebcad4f6bd1073a62952441bbf
jtardioli <joshua.tardioli@gmail.com>
2021-06-29 11:23:21 -0400

    Set up webpack with babel and set up seperate components folder.

---
+++ indecision-app/src/components/AddOption.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+//AddOption
+export default class AddOption extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleAddOption = this.handleAddOption.bind(this)
+        this.state = {
+            error: undefined
+        }
+    }
+    handleAddOption(e) {
+        e.preventDefault()
+        const option = e.target.elements.addOption.value.trim()
+        const error = this.props.handleAddOption(option)
+        this.setState(() => ({error})) 
+        
+        if(!error) {
+            e.target.elements.addOption.value = ''
+        }
+    }
+    render () {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleAddOption}>
+                <input name="addOption" placeholder="Add an option"/>
+                <button>Add</button>
+                
+                </form>
+            </div>
+        )
+    }
+}
+
