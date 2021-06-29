1ae7e92fac9660fcc6509ca50a0f25cd56e8b9ad
jtardioli <joshua.tardioli@gmail.com>
2021-06-29 12:00:36 -0400

    Finished section 6: webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,4 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
+
+
+
+
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+
+class Oldsyntax {
+    constructor() {
+        this.name = 'mike'
+    }
+}
+
+const oldSyntax = new Oldsyntax() 
+console.log(oldSyntax)
+
+//----------------
+
+class NewSyntax {
+    name = 'bob'
+}
+
+const newSyntax = new NewSyntax()
+console.log(newSyntax)
